9f7d85275b72ebdbe513f8bef3c2b5f0a13b03c0
Krzysztof <krzysiurr4@gmail.com>
2025-01-25 19:19:47 +0100

    Add files via upload

--- Shop/myshop/myshop/urls.py
+++ Shop/myshop/myshop/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from products.views import home
+#from products.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- Shop/myshop/products/views.py
+++ Shop/myshop/products/views.py
@@ -4,3 +4,8 @@ from .models import Product
 def home(request):
     products = Product.object.all()
     return render(request, 'products/home.html', {'products':products})
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, World!")
